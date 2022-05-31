376c1d431dcb6cc26984ed3945db99304043ec40
Jessie Zhang <jessiezhang98@gmail.com>
2022-05-31 16:55:55 +0100

    add shhh

--- js/index.js
+++ js/index.js
@@ -1,7 +1,75 @@
 let scrolled = 0;
+let access = sessionStorage.getItem('access');
 
 $(document).ready(() => {
 
+    handleScroll();
+    if (mobile == false) showThumbnails();
+
+    if (access == 1) {
+        $(".project.secret").css("display", "flex");
+        $(".project").css("height", "12.5%");
+    } else if (!access) {
+        onKonamiCode(() => {
+            sessionStorage.setItem('access', 1);
+            $(".project.secret").css("display", "flex");
+            $(".project").css("height", "12.5%");
+        });
+    }
+});
+
+function showThumbnails() {
+    // song light
+    $(".song-light").hoverIntent(() => {
+        $(".thumbnail-container.song-light").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.song-light").css("display", "none");
+    });
+
+    // speech bubbles
+    $(".speech-bubbles").hoverIntent(() => {
+        $(".thumbnail-container.speech-bubbles").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.speech-bubbles").css("display", "none");
+    });
+
+    // poet
+    $(".poet").hoverIntent(() => {
+        $(".thumbnail-container.poet").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.poet").css("display", "none");
+    });
+
+    // bloom
+    $(".bloom").hoverIntent(() => {
+        $(".thumbnail-container.bloom").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.bloom").css("display", "none");
+    });
+
+    // sonance
+    $(".sonance").hoverIntent(() => {
+        $(".thumbnail-container.sonance").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.sonance").css("display", "none");
+    });
+
+    // bbc habitat
+    $(".bbc-habitat").hoverIntent(() => {
+        $(".thumbnail-container.bbc-habitat").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.bbc-habitat").css("display", "none");
+    });
+
+    // technonatural futures
+    $(".technonatural-futures").hoverIntent(() => {
+        $(".thumbnail-container.technonatural-futures").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.technonatural-futures").css("display", "none");
+    });
+}
+
+function handleScroll() {
     if ($(window).scrollTop() >= $(window).innerHeight() * 0.94 - 100) {
         $('.projects-title').text("WORK ↑");
         scrolled = 1;
@@ -33,55 +101,26 @@ $(document).ready(() => {
         }
     });
 
-    if (mobile == false) {
+    window.addEventListener("keydown", function(e) {
+        if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+            e.preventDefault();
+        }
+    }, false);
+}
 
-        // song light
-        $(".song-light").hoverIntent(() => {
-            $(".thumbnail-container.song-light").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.song-light").css("display", "none");
-        });
-
-        // speech bubbles
-        $(".speech-bubbles").hoverIntent(() => {
-            $(".thumbnail-container.speech-bubbles").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.speech-bubbles").css("display", "none");
-        });
-
-        // poet
-        $(".poet").hoverIntent(() => {
-            $(".thumbnail-container.poet").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.poet").css("display", "none");
-        });
-
-        // bloom
-        $(".bloom").hoverIntent(() => {
-            $(".thumbnail-container.bloom").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.bloom").css("display", "none");
-        });
-
-        // sonance
-        $(".sonance").hoverIntent(() => {
-            $(".thumbnail-container.sonance").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.sonance").css("display", "none");
-        });
-
-        // bbc habitat
-        $(".bbc-habitat").hoverIntent(() => {
-            $(".thumbnail-container.bbc-habitat").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.bbc-habitat").css("display", "none");
-        });
-
-        // technonatural futures
-        $(".technonatural-futures").hoverIntent(() => {
-            $(".thumbnail-container.technonatural-futures").css("display", "block");
-        }, () => {
-            $(".thumbnail-container.technonatural-futures").css("display", "none");
-        });
-    }
-});
+function onKonamiCode(cb) {
+    const konami = [ "ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+    let index = 0;
+    document.addEventListener("keydown", (e) => {
+        const key = e.key;
+        if (key === konami[index]) {
+            index++;
+            if (index === konami.length) {
+                index = 0;
+                return cb();
+            }
+        } else {
+            index = 0;
+        }
+    });
+}
---
+++ js/shhh.js
@@ -0,0 +1,43 @@
+let access = sessionStorage.getItem('access');
+
+if (access == 1) {
+    $("#msg").css("display", "none");
+} else if (!access) {
+    $("#msg").css("display", "block");
+}
+
+$(document).ready(() => {
+    if (mobile == false) showThumbnails();
+});
+
+function showThumbnails() {
+
+    // waste age
+    $(".waste-age").hoverIntent(() => {
+        $(".thumbnail-container.waste-age").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.waste-age").css("display", "none");
+    });
+
+    // anyone
+    $(".anyone").hoverIntent(() => {
+        $(".thumbnail-container.anyone").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.anyone").css("display", "none");
+    });
+
+    // spin unreleased
+    $(".spin-unreleased").hoverIntent(() => {
+        $(".thumbnail-container.spin-unreleased").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.spin-unreleased").css("display", "none");
+    });
+
+    // salt & stone
+    $(".saltandstone").hoverIntent(() => {
+        $(".thumbnail-container.saltandstone").css("display", "block");
+    }, () => {
+        $(".thumbnail-container.saltandstone").css("display", "none");
+    });
+
+}
